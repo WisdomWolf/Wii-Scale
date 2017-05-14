95019e05d215d2e2764a8b3de3cb1cbc300d7b2a
WisdomWolf <wisdomwolf@gmail.com>
2017-05-13 20:29:54 -0400

    added test connection code to bt_test.py

--- wii-scale/bt_test.py
+++ wii-scale/bt_test.py
@@ -1,5 +1,17 @@
 from bluetoothctl import Bluetoothctl
+import time
 
 b = Bluetoothctl()
-
-print(b.get_available_devices())
+b.start_scan()
+time.sleep(10)
+devices = b.get_available_devices()
+print('Available Devices:')
+for device in devices:
+    print('\t{}'.format(device['name']))
+    if 'nintendo' in device['name'].lower():
+        print('Found Nintendo device: {} {}'.format(device['name'], device['mac_address']))
+        b.pair(device['mac_address'])
+        status = b.connect(device['mac_address'])
+        print('Connected: {}'.format(status))
+        break
+print(b.get_paired_devices())
